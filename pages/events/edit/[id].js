9d143316b3af5c7ed86715286ec5ec7b5ff9c938
ngwatso <ngwatson79@yahoo.com>
2021-09-15 21:17:17 -0400

    added image upload

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -7,6 +7,8 @@ import { FaImage } from 'react-icons/fa';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Layout from '@/components/Layout';
+import Modal from '@/components/Modal';
+import ImageUpload from '@/components/ImageUpload';
 import { API_URL } from '@/config/index';
 import styles from '@/styles/Form.module.css';
 import { useState } from 'react';
@@ -28,6 +30,8 @@ export default function EditEventPage({ evt }) {
 	const [imagePreview, setImagePreview] = useState(
 		evt.image ? evt.image.formats.thumbnail.url : null
 	);
+
+	const [showModal, setShowModal] = useState(false);
 
 	const router = useRouter();
 
@@ -62,6 +66,13 @@ export default function EditEventPage({ evt }) {
 	const handleInputChange = (e) => {
 		const { name, value } = e.target;
 		setValues({ ...values, [name]: value });
+	};
+
+	const imageUploaded = async (e) => {
+		const res = await fetch(`${API_URL}/events/${evt.id}`);
+		const data = await res.json();
+		setImagePreview(data.image.formats.thumbnail.url);
+		setShowModal(false);
 	};
 
 	return (
@@ -212,10 +223,25 @@ export default function EditEventPage({ evt }) {
 			)}
 
 			<div>
-				<button className="btn-secondary">
+				<button
+					onClick={() =>
+						setShowModal(true)
+					}
+					className="btn-secondary"
+				>
 					<FaImage /> Set Image
 				</button>
 			</div>
+
+			<Modal
+				show={showModal}
+				onClose={() => setShowModal(false)}
+			>
+				<ImageUpload
+					evtId={evt.id}
+					imageUploaded={imageUploaded}
+				/>
+			</Modal>
 		</Layout>
 	);
 }
