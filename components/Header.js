7bf0a8a5f920d929120e99fb7e7e21ed15fe1c65
ngwatso <ngwatson79@yahoo.com>
2021-09-16 11:36:22 -0400

    set up context state management AuthContext

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,15 @@
  * @prettier
  */
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { useContext } from 'react';
 import Link from 'next/link';
 import styles from '@/styles/Header.module.css';
 import Search from './Search';
+import AuthContext from '@/context/AuthContext';
 
 export default function Header() {
+	const { user, logout } = useContext(AuthContext);
+
 	return (
 		<header className={styles.header}>
 			<div className={styles.logo}>
@@ -26,22 +30,50 @@ export default function Header() {
 							</a>
 						</Link>
 					</li>
-					<li>
-						<Link href="/events/add">
-							<a>
-								Add
-								Event
-							</a>
-						</Link>
-					</li>
-					<li>
-						<Link href="/account/login">
-							<a className="btn-secondary btn-icon">
-								<FaSignInAlt />
-								Login
-							</a>
-						</Link>
-					</li>
+					{user ? (
+						// If logged in
+						<>
+							<li>
+								<Link href="/events/add">
+									<a>
+										Add
+										Event
+									</a>
+								</Link>
+							</li>
+							<li>
+								<Link href="/account/dashboard">
+									<a>
+										Dashboard
+									</a>
+								</Link>
+							</li>
+							<li>
+								<button
+									onClick={() =>
+										logout()
+									}
+									className="btn-secondary btn-icon"
+								>
+									<FaSignOutAlt />
+									Log
+									Out
+								</button>
+							</li>
+						</>
+					) : (
+						// If logged out
+						<>
+							<li>
+								<Link href="/account/login">
+									<a className="btn-secondary btn-icon">
+										<FaSignInAlt />
+										Login
+									</a>
+								</Link>
+							</li>
+						</>
+					)}
 				</ul>
 			</nav>
 		</header>
--- pages/account/login.js
+++ pages/account/login.js
@@ -8,19 +8,17 @@ import { useState, useEffect, useContext } from 'react';
 import Link from 'next/link';
 import Layout from '@/components/Layout';
 import styles from '@/styles/AuthForm.module.css';
+import AuthContext from '@/context/AuthContext';
 
 export default function LoginPage() {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 
+	const { login, error } = useContext(AuthContext);
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
-
-		if (password !== passwordConfirm) {
-			toast.error('Passwordsdo not match');
-			return;
-		}
-		console.log({ username, email, password });
+		login({ email, password });
 	};
 
 	return (
